a9ede020bc92a43466e644fd6e8edb0c2231131f
Leo Ribeiro <leordev@gmail.com>
2020-06-18 23:03:24 -0400

    adiciona chakra

--- webapp/pages/_app.tsx
+++ webapp/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/react-hooks";
+import { ThemeProvider, CSSReset, theme } from "@chakra-ui/core";
 
 import { useApollo } from "../lib/apolloClient";
 import "../styles/global.css";
@@ -7,9 +8,12 @@ import "../styles/global.css";
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   const apolloClient = useApollo(pageProps.initialApolloState);
   return (
-    <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />;
-    </ApolloProvider>
+    <ThemeProvider theme={theme}>
+      <CSSReset />
+      <ApolloProvider client={apolloClient}>
+        <Component {...pageProps} />;
+      </ApolloProvider>
+    </ThemeProvider>
   );
 };
 
